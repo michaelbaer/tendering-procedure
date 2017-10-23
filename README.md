e487898736ca124d3f8d3078d42fa3b988322cc0
Michael Bär <kontakt@michaelbaer.net>
2017-10-23 22:11:37 +0200

    edit setup.s

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tendering-procedure
 Hyperledger fabric example project for a tendering procedure
+
+
--- tender/setup.sh
+++ tender/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# 1. step generate keys with configgen and xrypto-config.yaml
+# ==========
+# Generate crypto stuff and configuration transactions for channel and peers
+# ==========
+
+# 1. step generate keys with cryptogen and crypto-config.yaml
 cryptogen generate --output="crypto-config" --config=crypto-config.yaml
 
 # 2. step configtxgen genesis block
-mkdir channel-artifacts
 export FABRIC_CFG_PATH=$PWD
 configtxgen -profile TenderOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
@@ -12,7 +15,7 @@ configtxgen -profile TenderOrgsOrdererGenesis -outputBlock ./channel-artifacts/g
 export CHANNEL_NAME=tenderchannel
 configtxgen -profile TenderOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
 
-# 4. step peer tx
+# 4. step peer tx's
 configtxgen -profile TenderOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/AnnouncerOrgAnchors.tx -channelID $CHANNEL_NAME -asOrg AnnouncerOrg
 configtxgen -profile TenderOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Bidder1OrgAnchors.tx -channelID $CHANNEL_NAME -asOrg Bidder1Org
 configtxgen -profile TenderOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Evaluator1OrgAnchors.tx -channelID $CHANNEL_NAME -asOrg Evaluator1Org
